f70c1924c433980dcd2fda809d28bd7366bb016f
Sanath S Aithal <69667375+sanathsaithal@users.noreply.github.com>
2021-11-08 00:04:00 +0530

    Delete spring/dependency_injection/bus directory

--- spring/dependency_injection/bus/src/main/java/com/wolken/bus/dao/BusDao.java
+++
@@ -1,9 +0,0 @@
-package com.wolken.bus.dao;
-
-import com.wolken.bus.entity.BusEntity;
-
-public interface BusDao {
-
-	String save(BusEntity entity);
-
-}
--- spring/dependency_injection/bus/src/main/java/com/wolken/bus/services/BusService.java
+++
@@ -1,9 +0,0 @@
-package com.wolken.bus.services;
-
-import com.wolken.bus.dto.BusDto;
-
-public interface BusService {
-
-	String validateAndSave(BusDto dto);
-
-}
